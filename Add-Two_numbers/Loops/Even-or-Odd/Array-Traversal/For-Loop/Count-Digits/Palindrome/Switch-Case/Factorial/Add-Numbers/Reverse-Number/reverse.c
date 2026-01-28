fe9cba3ef6eae5a67464efe587e4513b1125dc9f
dhruvatr <dhruvadhruvakumar160-jpg>
2026-01-28 21:05:12 +0530

    Added swap numbers program

---
+++ Add-Two_numbers/Loops/Even-or-Odd/Array-Traversal/For-Loop/Count-Digits/Palindrome/Switch-Case/Factorial/Add-Numbers/Reverse-Number/reverse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main() {
+    int n = 5648
+    int rev = 0;
+
+    while(n != 0){
+        rev = rev * 10 + (n % 10);
+        n = n / 10;
+
+    }
+
+    printf("Reversed Number = %d", rev);
+    return 0;
+ }
